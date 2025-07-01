4e9b64528e1056eb2e8a1174ba70db9e2a708c10
randyonion <randyonion679@gmail.com>
2025-07-01 22:50:29 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,12 +18,16 @@ module tt_um_example (
 
 
     wire reset = ~rst_n;
+    assign uio_out = 0;
+    assign uio_oe = 0;
+    wire _unuse = &(ena,uio_in,1'b0);
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             uo_out <= 0;
         end else begin
-            uo_out <= ui_in[7:4] + ui_in[3:0];
+            uo_out[3:0] <= ui_in[7:4] + ui_in[3:0];
+            uo_out[7:4] <= 0;
         end
     end
 
